4ab2e61a47de8519859e72d4bffe165dbac22ff4
Carol-88 <gralleta@gmail.com>
2023-10-27 17:15:20 +0200

    inicio styles de header y hagoun global para el reset

--- client-benkyo/src/pages/App.js
+++ client-benkyo/src/pages/App.js
@@ -2,14 +2,15 @@ import React from 'react'
 import Game from '@/components/game/Game'
 import Header from '@/components/header/Header'
 import Footer from '@/components/footer/Footer'
+import styles from './global.module.css'
 
 const App = () => {
   return (
-    <>       
+    <div className={styles.globalreset}>       
       <Header/>
       <Game/>          
       <Footer/>
-    </>
+    </div>
   )
 }
 
